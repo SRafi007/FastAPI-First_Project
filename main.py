1c103b5163621b603ef95f687c0e53f5dab41e71
SRafi007 <sadmansakib.rafiaiub@gmail.com>
2023-08-01 03:16:07 +0600

    first commit

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+@app.get("/val/{param_Val}")
+async def getParamVal(param_Val:int):
+    return {"param":param_Val}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
